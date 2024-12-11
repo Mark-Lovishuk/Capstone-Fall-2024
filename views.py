6d077604ae6e7538beffcd3e56adbc51306fc90c
Mark-Lovishuk <marklovishuk@gmail.com>
2024-12-11 12:04:04 -0500

    Update views.py

--- views.py
+++ views.py
@@ -1,4 +1,10 @@
+# Import the render function from Django to render HTML templates
 from django.shortcuts import render
 
+# Define the view function for the 'index' page
+# The 'request' parameter represents the HTTP request that triggered this view
 def index(request):
+    
+    # Render the 'index.html' template and return the resulting HTTP response
+    #Giving the template context
     return render(request, 'index.html')
