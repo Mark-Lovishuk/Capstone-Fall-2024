57e2da1f189f0b87c738bf6fa78e392b9fcdfff2
Mark-Lovishuk <marklovishuk@gmail.com>
2024-12-11 14:37:11 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# this references the AppConfig class defined above
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
